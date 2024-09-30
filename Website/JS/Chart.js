97aa28f2e68f24d5c162a64f478f7d5d702f7c3f
Jonatan Cederberg <jonceder@student.chalmers.se>
2024-09-30 13:23:11 +0200

    Charts are switchable and render on pageload

--- Website/JS/Chart.js
+++ Website/JS/Chart.js
@@ -1,9 +1,21 @@
 
+const date = new Date();
+const year = date.getFullYear();
+const month = date.getMonth();
+const day = date.getDate();
+const hour = date.getHours();
+const minute = date.getMinutes();
+var today = date.toISOString().split('T')[0];
+
+
+
+
+
 const data2 = {
-    labels: ["January", "February", "March", "April", "May", "June", "July"],
+    labels: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
     datasets: [{
-        label: "Energy usage (current year)",
-        data: [0, 10, 5, 2, 20, 30, 45],
+        label: "Energy usage ("+ year +")",
+        data: [0, 10, 5, 2, 20, 30, 45, 50, 60, 50, 55, 55],
         fill: true,
         borderColor: "rgb(75, 192, 192)",
         tension: 0.1
@@ -13,7 +25,7 @@ const data2 = {
 const data = {
     labels: ["00", "02", "04", "06", "08", "10", "12", "14", "16", "18", "20", "22"],
     datasets: [{
-        label: "Energy usage (today)",
+        label: "Energy usage ("+ today +")",
         data: [1, 2, 4, 3, 1, 3, 5, 3, 1, 3, 5, 3],
         fill: true,
         borderColor: "rgb(75, 192, 192)",
@@ -30,10 +42,37 @@ console.log(CHART);
 let lineChart = new Chart(CHART, {
     type: "line",
     data: data,
+    options: {
+        legend: {
+            onClick: null
+        }
+    }
 });
 let lineChart2 = new Chart(CHART2, {
     type: "line",
     data: data2,
+    options: {
+        legend: {
+            onClick: null
+        }
+    }
 });
+
+function openChart(evt, chartType) {
+    var i, chartTab, timeTabButton;
+
+    chartTab = document.getElementsByClassName("chartTab");
+    for (i = 0; i < chartTab.length; i++) {
+        chartTab[i].style.display = "none";
+    }
+
+    timeTabButton = document.getElementsByClassName("timeTabButton");
+    for (i = 0; i < timeTabButton.length; i++) {
+        timeTabButton[i].className = timeTabButton[i].className.replace(" active", "");
+    }
+
+    document.getElementById(chartType).style.display = "block";
+    evt.currentTarget.className += " active";
+}
 
 
